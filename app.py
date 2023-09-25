2d2c31d944f608566ade57ad0107e691ed8f45ac
arvkevi <arvkevi@gmail.com>
2023-09-24 21:14:37 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,203 @@
+import datetime
+import os
+
+import pandas as pd
+import psycopg2
+import streamlit as st
+from st_aggrid import AgGrid, GridOptionsBuilder
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+
+CONNECTION_URI = os.environ.get("CONNECTION_URI")
+SPOTIFY_CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID")
+SPOTIFY_CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET")
+
+st.set_page_config(
+    page_title="MLB Walkup Songs to Spotify Playlist",
+    page_icon="baseball",
+    layout="wide",
+)
+
+
+@st.cache_data()
+def get_mlb_walkup_data(
+    connection_uri: str,
+    walkup_date: datetime.date = datetime.date.today(),
+):
+    """
+    Query the MLB walkup songs on a given date
+    """
+    try:
+        conn = psycopg2.connect(connection_uri)
+    except psycopg2.OperationalError as conn_error:
+        st.error("Unable to connect!\n{0}").format(conn_error)
+
+    walkup_date = walkup_date.strftime("%Y-%m-%d")
+
+    cur = conn.cursor()
+    cur.execute(
+        """
+        SELECT * FROM mlb_walk_up_songs
+        WHERE walkup_date BETWEEN %s AND %s
+        """,
+        (walkup_date, walkup_date),
+    )
+    walkup_song_data = cur.fetchall()
+    conn.close()
+    column_names = [
+        "player",
+        "song_name",
+        "song_artist",
+        "team",
+        "walkup_date",
+        "spotify_uri",
+        "explicit",
+    ]
+    return pd.DataFrame(walkup_song_data, columns=column_names)
+
+
+# Title and gif
+title_, gif = st.columns([0.60, 0.40])
+title_.title("Create a Spotify playlist from MLB walkup songs")
+gif.markdown(
+    """
+    <iframe src="https://giphy.com/embed/XTGstcX6TBdlFk7Cd8" width="320" height="180" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/mlb-sports-baseball-XTGstcX6TBdlFk7Cd8"></a></p>
+    """,
+    unsafe_allow_html=True,
+)
+# Date picker and metrics
+col1, col2, col3, col4, col5 = st.columns([0.2] * 5, gap="large")
+date = col1.date_input(
+    "Choose a date :calendar: : ",
+    value=datetime.date.today(),
+    min_value=datetime.date(2023, 9, 23),
+)
+
+data = get_mlb_walkup_data(CONNECTION_URI, date)
+n_spotify = data["spotify_uri"].notnull().sum()
+data = data[["team", "player", "song_name", "song_artist", "explicit", "spotify_uri"]]
+data.sort_values(by=["team", "player"], inplace=True)
+
+col2.metric("Songs", data["song_name"].nunique())
+col3.metric("Songs in Spotify", n_spotify)
+col4.metric("Players", data["player"].nunique())
+col5.metric("Teams", data["team"].nunique())
+
+selected_rows = None
+
+if data.empty:
+    st.warning("No walkup songs found for this date.")
+else:
+    gb = GridOptionsBuilder.from_dataframe(data)
+    gb.configure_selection(
+        "multiple",
+        use_checkbox=True,
+        header_checkbox=True,
+        header_checkbox_filtered_only=True,
+        rowMultiSelectWithClick=True,
+    )
+    gb.configure_pagination(paginationAutoPageSize=False, paginationPageSize=10)
+    gb.configure_column(field="spotify_uri", hide=True)
+    gb.configure_column(field="team", header_name="Team")
+    gb.configure_column(field="player", header_name="Player Name")
+    gb.configure_column(field="song_name", header_name="Song Name")
+    gb.configure_column(field="song_artist", header_name="Song Artist")
+    gb.configure_column(
+        field="explicit",
+        cell_renderer="agCheckboxCellRenderer",
+        cell_renderer_params={
+            "suppressTrueText": True,
+            "suppressFalseText": True,
+            "disabled": True,
+        },
+        cell_style={"textAlign": "center"},
+    )
+    gb.configure_grid_options(
+        **{
+            "statusBar": {
+                "statusPanels": [
+                    {"statusPanel": "agTotalRowCountComponent", "align": "left"},
+                    {"statusPanel": "agSelectedRowCountComponent", "align": "center"},
+                ]
+            },
+        }
+    )
+    go = gb.build()
+
+    grid = AgGrid(
+        data,
+        gridOptions=go,
+        fit_columns_on_grid_load=True,
+        theme="material",
+        key="grid",
+    )
+    selected_rows = grid["selected_rows"]
+    selected_rows_df = pd.DataFrame(selected_rows)
+
+# if st.button("Create Spotify Playlist from Selected Songs"):
+with st.form("playlist-form", clear_on_submit=False):
+    st.subheader("Create Spotify playlist from selected songs")
+    col, buff, buff2 = st.columns([0.2, 0.6, 0.2])
+    playlist_name = col.text_input("Playlist Name", value=f"MLB Walkup Songs {date}")
+
+    if selected_rows:
+        selected_rows_df.drop(columns=["_selectedRowNodeInfo"], inplace=True)
+        st.dataframe(
+            selected_rows_df.rename(
+                columns={
+                    "team": "Team",
+                    "player": "Player Name",
+                    "song_name": "Song Name",
+                    "song_artist": "Song Artist",
+                    "explicit": "Explicit",
+                }
+            ),
+            hide_index=True,
+        )
+
+    submit = st.form_submit_button("Create Playlist")
+    if submit:
+        try:
+            spotify = spotipy.Spotify(
+                oauth_manager=SpotifyOAuth(
+                    client_id=SPOTIFY_CLIENT_ID,
+                    client_secret=SPOTIFY_CLIENT_SECRET,
+                    redirect_uri="http://localhost:8080",
+                    scope="playlist-modify-private",
+                )
+            )
+        except Exception as e:
+            st.error(f"Error authenticating with Spotify: {e}")
+
+        if spotify:
+            st.success("Successfully authenticated with Spotify!")
+            try:
+                mlb_walkup_playlist = spotify.user_playlist_create(
+                    user=spotify.current_user()["id"],
+                    name=playlist_name,
+                    public=False,
+                    collaborative=False,
+                    description="MLB Walkup Songs",
+                )
+            except Exception as e:
+                st.error(f"Error creating playlist: {e}")
+
+            if mlb_walkup_playlist:
+                st.success("Successfully created playlist!")
+                try:
+                    spotify.user_playlist_add_tracks(
+                        user=spotify.current_user()["id"],
+                        playlist_id=mlb_walkup_playlist["id"],
+                        tracks=selected_rows_df["spotify_uri"].dropna().tolist(),
+                    )
+                except Exception as e:
+                    st.error(f"Error adding tracks to playlist: {e}")
+
+                st.success("Successfully added tracks to playlist!")
+                st.write(
+                    f"Check out your playlist [here]({mlb_walkup_playlist['external_urls']['spotify']})."
+                )
+        else:
+            st.error("Unable to authenticate with Spotify.")
---
+++ scraper.py
@@ -0,0 +1,122 @@
+import datetime
+
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import psycopg2
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+from sqlalchemy import create_engine
+
+import time
+import sys
+
+if __name__ == "__main__":
+    
+    CONNECTION_URI = sys.argv[1]
+    SPOTIFY_CLIENT_ID = sys.argv[2]
+    SPOTIFY_CLIENT_SECRET = sys.argv[3]
+
+    mlb_site = "https://mlb.com"
+    music_endpoint = "ballpark/music"
+
+    bs = BeautifulSoup(requests.get(f"{mlb_site}/fans").text, "html.parser")
+
+    team_links = []
+    links = bs.find_all("a", {"data-parent": "Teams"}, href=True)
+    for link in links:
+        team_links.append(f"{mlb_site}{link['href']}/{music_endpoint}")
+
+    team_songs = {}
+    for team_link in team_links:
+        team_name = team_link.split("/")[-3]
+        bsteam = BeautifulSoup(requests.get(team_link, timeout=60).text, "html.parser")
+
+        try:
+            players = bsteam.find("div", {"class": "p-forge-list"}).findAll(
+                "div", {"class": "p-featured-content__body"}
+            )
+            player_songs = {}
+            for player in players:
+                player_name = player.find("div", {"class": "u-text-h4"}).text.strip()
+
+                p_tag = player.find("div", {"class": "p-featured-content__text"}).find(
+                    ["p", "span"]
+                )
+
+                # Initialize an empty string to hold the text from the <p> tag
+                p_text_only = ""
+
+                # Loop through the elements inside the <p> tag
+                for content in p_tag.contents:
+                    if content.name is None:  # Text, not a tag
+                        p_text_only += content
+
+                # Remove leading and trailing whitespace
+                p_text_only = p_text_only.strip()
+                em_tag = p_tag.find("em") if p_tag else None
+                i_tag = p_tag.find("i") if p_tag else None
+
+                if em_tag:
+                    song_name = em_tag.text
+                elif i_tag:
+                    song_name = i_tag.text
+                else:
+                    song_name = ""
+
+                song_artist = (
+                    p_tag.text.replace(song_name, "").replace("by", "").strip()
+                )
+
+                player_songs[player_name] = {
+                    "song_name": song_name,
+                    "song_artist": song_artist,
+                }
+
+            team_songs[team_name] = player_songs
+        except Exception as e:
+            print(f"Error with {team_name}: {e}")
+
+    spotify_search = spotipy.Spotify(
+        client_credentials_manager=SpotifyClientCredentials(
+            client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET
+        )
+    )
+
+    for team in team_songs:
+        for player in team_songs[team]:
+            song_name = team_songs[team][player]["song_name"].strip().replace("\n", " ")
+            song_artist = (
+                team_songs[team][player]["song_artist"].strip().replace("\n", " ")
+            )
+            if song_name and song_artist:
+                results = spotify_search.search(
+                    q=f"track:{song_name} artist:{song_artist}", type="track", limit=1
+                )
+                if results["tracks"]["items"]:
+                    team_songs[team][player]["spotify_id"] = results["tracks"]["items"][
+                        0
+                    ]
+                else:
+                    team_songs[team][player]["spotify_id"] = None
+            else:
+                team_songs[team][player]["spotify_id"] = None
+            time.sleep(0.2)
+
+    df = pd.DataFrame()
+    for team in team_songs:
+        dfteam = pd.DataFrame(team_songs[team]).T.reset_index()
+        dfteam["team"] = team
+        dfteam.rename(columns={"index": "player"}, inplace=True)
+        df = pd.concat([df, dfteam], ignore_index=True, axis=0)
+
+    df["walkup_date"] = datetime.date.today()
+    df["spotify_uri"] = df["spotify_id"].apply(lambda x: x["uri"] if x else None)
+    df["explicit"] = df["spotify_id"].apply(lambda x: x["explicit"] if x else None)
+    df.drop(columns=["spotify_id"], inplace=True)
+    engine = create_engine(CONNECTION_URI.replace("postgresql", "postgresql+psycopg2"))
+    df.to_sql('mlb_walk_up_songs', engine, if_exists='append', index=False)
+
+    sys.stdout.write(f"Successfully scraped {df.loc[df['spotify_uri'].notnull()].shape[0]} of {df.shape[0]} MLB walk-up songs.")
