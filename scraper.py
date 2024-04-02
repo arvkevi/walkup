a6fb87777ab9501e77ccd8b46bd93274e58ad272
arvkevi <arvkevi@gmail.com>
2024-04-01 22:01:39 -0400

    Use the right key in alt method for artists

--- scraper.py
+++ scraper.py
@@ -147,7 +147,7 @@ if __name__ == "__main__":
                     for song in songs:
                         song_name = song.find("div", {"class": "player-walkup-music__song--content--songname"}).get_text()
                         artist_name = song.find("div", {"class": "player-walkup-music__song--content--artistname"}).get_text()
-                        player_songs[player_name].append({"song_name": song_name, "artist_name": artist_name})
+                        player_songs[player_name].append({"song_name": song_name, "song_artist": artist_name})
 
                 team_songs[team_name] = player_songs
 
